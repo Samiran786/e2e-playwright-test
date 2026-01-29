28f4b1dd08b0e0bfe27fe7e9823d9e78bfd69dd9
Samiran786 <bsamibane786@gmail.com>
2026-01-30 01:41:07 +0530

    playwright-module:3 - Writing first test script

---
+++ tests/uibasics.spec.js
@@ -0,0 +1,30 @@
+import {test} from '@playwright/test';
+
+// 'browser' parameter is part of 'test' function. This parameter controls browser context
+test('First Playwright test',async ({browser})=>{
+    
+    /* 
+        newContext Creates a new browser context. It won't share cookies/cache 
+        with other browser contexts.
+        we can control the cookies/cache of one browser context by this function
+    */
+    const context = await browser.newContext();
+
+    // newPage creates a new page inside of the browser context
+    const page = await context.newPage();
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+    await context.close();
+    await browser.close();
+});
+
+/* 
+    If our main goal is page and not controlling whole browser context then we can directly use the
+    'page' parameter of the 'test' function.
+*/
+test('Optimized First Playwright test',async ({page})=>{
+    
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+
+});
